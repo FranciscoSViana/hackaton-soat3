97e36e4e55d1802172d87a88d325a1efb176e9a3
Vinicius Júnior <vinni.jr@gmail.com>
2024-07-17 21:51:20 -0300

    first commit

---
+++ src/main/java/com/soat3/hackaton/atendmed/AtendmedApplication.java
@@ -0,0 +1,13 @@
+package com.soat3.hackaton.atendmed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtendmedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtendmedApplication.class, args);
+	}
+
+}
