4e290748b7d63126d1b043a35c543da42eaf4dd5
Vinicius Júnior <vinni.jr@gmail.com>
2024-07-19 13:10:11 -0300

    Adicionando

---
+++ src/main/java/com/soat3/hackaton/atendmed/infrastructure/repository/medico/AgendaRepository.java
@@ -0,0 +1,9 @@
+package com.soat3.hackaton.atendmed.infrastructure.repository.medico;
+
+import com.soat3.hackaton.atendmed.domain.model.medico.AgendaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface AgendaRepository extends JpaRepository<AgendaModel, UUID> {
+}
